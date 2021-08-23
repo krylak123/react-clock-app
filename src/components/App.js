f7cadf5f977c89189386568310e7648d2aeac7b5
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-08-23 10:31:57 +0200

    setup project boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from '../styles/GlobalStyles';
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <div>
+      <p>React App</p>
+    </div>
+  </>
+);
+
+export default App;
